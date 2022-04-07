37161d123ca06bbdc0864915ec686f9d9a488851
nikon <nikon2127@gmail.com>
2022-04-07 22:22:31 +0300

    Add task 5 smallest multiple of range of numbers

---
+++ task_5_smallest_multiple_of_range_of_numbers.py
@@ -0,0 +1,19 @@
+# 2520 самое маленькое число, которое делится нацело на все числа от 1 до 10
+# Какое самое маленькое число делится нацело на все числа от 1 до 20?
+
+def smallest_multiple_of_range(start, end):
+    result = 2
+    while True:
+        i = start
+        while i <= end:
+            if result % i != 0:
+                break
+            if i == end:
+                return result
+            i += 1
+        result += 2
+
+
+print(smallest_multiple_of_range(1, 10))
+print(smallest_multiple_of_range(1, 15))
+print(smallest_multiple_of_range(1, 20))
